ba93609f60b6b6e8ad54a92beb3ae5486a334a33
ChaiUrs <mysorechaitra@gmail.com>
2019-09-12 16:08:54 -0700

    Fixed the editing feature and also made sure all the tests pass

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -45,24 +45,28 @@ describe("Form", () => {
 
 	it("can successfully save after trying to submit an empty student name", () => {
 		const onSave = jest.fn();
-		const { getByText, getByPlaceholderText, queryByText } = render(
-			<Form interviewers={interviewers} onSave={onSave} />
-		);
+		const {
+			getByText,
+			getByAltText,
+			getByPlaceholderText,
+			queryByText
+		} = render(<Form interviewers={interviewers} onSave={onSave} />);
 
 		fireEvent.click(getByText("Save"));
 
-		expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+		expect(getByText(/Student name cannot be blank/i)).toBeInTheDocument();
 		expect(onSave).not.toHaveBeenCalled();
 
 		fireEvent.change(getByPlaceholderText("Enter Student Name"), {
 			target: { value: "Lydia Miller-Jones" }
 		});
 
+		fireEvent.click(getByAltText("Sylvia Palmer"));
 		fireEvent.click(getByText("Save"));
 
-		expect(queryByText(/student name cannot be blank/i)).toBeNull();
+		expect(queryByText(/Student name cannot be blank/i)).toBeNull();
 		expect(onSave).toHaveBeenCalledTimes(1);
-		expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+		expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1, false);
 	});
 
 	it("calls onCancel and resets the input field", () => {
@@ -84,10 +88,8 @@ describe("Form", () => {
 
 		fireEvent.click(getByText("Cancel"));
 
-		expect(queryByText(/student name cannot be blank/i)).toBeNull();
-
+		expect(queryByText(/Student name cannot be blank/i)).toBeNull();
 		expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
-
 		expect(onCancel).toHaveBeenCalledTimes(1);
 	});
 });
